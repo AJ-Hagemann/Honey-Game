5af3878c084ebf6b3763708f6f56a3d59e008010
Christian Jackson <cjackson430@yahoo.com>
2022-04-18 18:24:57 -0500

    Created code that correctly identifies tiles of game board

--- Game Board.gd
+++ Game Board.gd
@@ -1,14 +1,5 @@
 extends Node2D
 
-onready var nav2d = $Navigation2D
-onready var character = $Character
-
-func _unhandled_input(event):
-	if not event is InputEventMouseButton:
-		return
-	if event.button_index != BUTTON_LEFT or not event.pressed:
-		print(event.global_position, $Navigation2D/HexTileMap.world_to_map(event.global_position))
-		return
-
-# note, can use global_position here; the video explains why you may want to do that
-	character.path = nav2d.get_simple_path(character.position, event.global_position)
+func _input(event):
+	if event.is_action_released("leftclick"):
+		print($HexTileMap.FindCell(get_global_mouse_position()))
