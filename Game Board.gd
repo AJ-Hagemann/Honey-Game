1b03278793e352382c2227564bdc7ec6b114fd8f
Christian Jackson <cjackson430@yahoo.com>
2022-04-20 09:43:28 -0500

    Finished board, started movement, starting to work on player winning and collecting resources

--- Game Board.gd
+++ Game Board.gd
@@ -1,5 +1,63 @@
 extends Node2D
 
-func _input(event):
-	if event.is_action_released("leftclick"):
-		print($HexTileMap.FindCell(get_global_mouse_position()))
+onready var cam = $GameCam
+onready var p1 = $Player1
+onready var p2 = $Player2
+onready var plabel = $HUD/HBoxContainer/PlayerLabel
+onready var spaceLabel = $HUD/HBoxContainer/SpaceLabel
+onready var moveBtn = $HUD/HBoxContainer/MoveButton
+onready var endBtn = $HUD/HBoxContainer/EndTurn
+var rng = RandomNumberGenerator.new()
+
+var nextPlayer = ["", "Player 2", "Player 1"]
+var currPlayerIdx = 1
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rng.randomize()
+	move_camera(p1)
+	GameState.currentPlayer = p1
+	GameState.currentPlayerLabel = "Player 1"
+	update_label()
+	
+
+
+# moves camera to parent
+func move_camera(p):
+	cam.get_parent().remove_child(cam)
+	p.add_child(cam)
+
+func update_label():
+	plabel.text = GameState.currentPlayerLabel
+
+func update_spaceLabel(space):
+	spaceLabel.text = str(space)	
+
+func _on_MoveButton_pressed():
+	moveBtn.disabled = true
+	GameState.currentPlayer.move(rng.randi_range(1,6))
+	yield(GameState.currentPlayer, 'movedone')
+	moveBtn.visible = false
+	endBtn.visible = true
+
+
+func _on_EndTurn_pressed():
+	$HUD/TurnSwitch/VBoxContainer/Label.text = nextPlayer[currPlayerIdx] + "'s Turn"
+	$HUD/TurnSwitch.visible = true
+
+
+func _on_Button_pressed():
+	GameState.currentPlayerLabel = nextPlayer[currPlayerIdx]
+	match currPlayerIdx:
+		1:
+			GameState.currentPlayer = p2
+			currPlayerIdx = 2
+		2:
+			GameState.currentPlayer = p1
+			currPlayerIdx = 1
+	update_spaceLabel(GameState.currentPlayer.space)
+	update_label()
+	move_camera(GameState.currentPlayer)
+	endBtn.visible=false
+	moveBtn.visible=true
+	moveBtn.disabled=false
+	$HUD/TurnSwitch.visible=false
--- Player.gd
+++ Player.gd
@@ -1,16 +1,44 @@
-extends Node
+extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var sprite = $Sprite
+export var player = 0;
+onready var tween = $Tween
+var sprites = [preload("res://Assets/p1.png"), preload("res://Assets/p2.png")]
+var space = 0
+var dir = Vector2.RIGHT
+var speed = 2
+var tilesize = 128
+
+signal movedone
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	sprite.texture = sprites[player]
+	position.x = 64
+	position.y = 64
 
+func move(spaces):
+	for n in spaces:
+		movespace()
+		yield(tween, "tween_completed")
+		GameState.update_spaceLabel(space)
+	emit_signal("movedone")
 
+func movespace():
+	match space:
+		0: dir = Vector2.RIGHT
+		11: dir = Vector2.DOWN
+		18: dir = Vector2.LEFT
+		29: dir = Vector2.UP
+	space = (space + 1) % 36
+	tween.interpolate_property(self, "position", position,
+	position + dir * tilesize, 1.0/speed, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
+	
+		
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
